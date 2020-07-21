683d1a99335b113e2233e088d71dc30d29c93825
Allison Sadin <aesadin@gmail.com>
2020-07-21 15:43:38 -0700

    create car constructor and properties of fields
    Co-authored-by: James Henager <jameschenager@gmail.com>
    Co-authored-by: SPdowns <spdowns@protonmail.com>

---
+++ CarDealership/Models/Car.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace CarDealership.Models
+{
+  public class Car
+  {
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
+    private static List<Car> _listOfCars = new List<Car> {};
+
+
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+
+    }
+  }
+}
